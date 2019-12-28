4f8b72ada755d7f2e3b44691cd6a747e1966a014
avadaneid <51731664+avadaneid@users.noreply.github.com>
2019-12-28 18:45:23 +0200

    Add existing file

---
+++ Project_CSV/App_Start/RouteConfig.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Project_CSV
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Main",
+                url: "Main/Main",
+                defaults: new { controller = "Main", action = "Main", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+               name: "Insert",
+               url: "Main/Insert",
+               defaults: new { controller = "Main", action = "Insert", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+            name: "Upload",
+            url: "Main/Upload",
+            defaults: new { controller = "Main", action = "Upload", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+            name: "Download",
+            url: "Main/Download",
+            defaults: new { controller = "Main", action = "Download", id = UrlParameter.Optional }
+            );      
+        }
+    }
+}
